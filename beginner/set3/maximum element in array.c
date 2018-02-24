8752f652fdb9903020eb1a822277f0fae22e74bf
Vanithamuthusamy <34671388+Vanithamuthusamy@users.noreply.github.com>
2018-02-24 19:03:16 +0530

    Create maximum element in array.c

---
+++ beginner/set3/maximum element in array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[100],max,i,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+	max=a[0];
+	for(i=0;i<n;i++)
+	{
+		
+		if(max<a[i])
+		{
+		max=a[i];	
+		}
+		
+	}
+	printf("%d",max);
+	return 0;
+}
